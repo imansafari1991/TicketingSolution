bc96c72e45aaa959fb198f82fd6f41e496eeac05
Iman Safari <iman.safari@gardenoflove.co>
2022-07-22 19:44:36 +0430

    add test method for get

--- TicketingSolution.API.Test/UnitTest1.cs
+++ TicketingSolution.API.Test/UnitTest1.cs
@@ -1,10 +1,26 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+using TicketingSolution.API.Controllers;
+
 namespace TicketingSolution.API.Test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Should_Return_Forcast_Results()
     {
+        //Arrange 
+        var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+        var controller=new WeatherForecastController(loggerMock.Object);
+        //Act
+        var result = controller.Get();
+
+        //Assert
+        result.ShouldNotBeNull();
+        result.Count().ShouldBeGreaterThan(1);
+       
+        ;
 
     }
 }
